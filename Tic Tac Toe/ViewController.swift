deb4ad8ad26ca93b42147d869f12f590bd2158a0
gregersundvall <gregersundvall@me.com>
2020-12-15 15:58:21 +0100

    asdf

--- Tic Tac Toe/ViewController.swift
+++ Tic Tac Toe/ViewController.swift
@@ -9,10 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var listOfPositions = [Int]()
+    var currentPlayer = 1
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+        
+        
+        for _ in 1...9 {
+            listOfPositions.append(0)
+        }
+        print(listOfPositions)
+        
+        
+        
+        listOfPositions[5] = 1
+        
+        print(listOfPositions)
+        
     }
+    
+    
 
 
 }
