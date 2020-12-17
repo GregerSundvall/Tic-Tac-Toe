8805046d3a38dda8810f131aac8abd8be9d1223d
gregersundvall <gregersundvall@me.com>
2020-12-17 15:57:21 +0100

    updated interface with name and wins etc

--- Tic Tac Toe/ViewController.swift
+++ Tic Tac Toe/ViewController.swift
@@ -10,8 +10,7 @@ import UIKit
 class ViewController: UIViewController {
 
     
-    @IBOutlet weak var labelp1: UILabel!
-    @IBOutlet weak var labelp2: UILabel!
+    
     @IBOutlet weak var button1: UIButton!
     @IBOutlet weak var button2: UIButton!
     @IBOutlet weak var button3: UIButton!
@@ -21,7 +20,14 @@ class ViewController: UIViewController {
     @IBOutlet weak var button7: UIButton!
     @IBOutlet weak var button8: UIButton!
     @IBOutlet weak var button9: UIButton!
-    
+    @IBOutlet weak var p1Background: UIView!
+    @IBOutlet weak var p1Label: UILabel!
+    @IBOutlet weak var p1Name: UILabel!
+    @IBOutlet weak var p1Wins: UILabel!
+    @IBOutlet weak var p2Background: UIView!
+    @IBOutlet weak var p2Label: UILabel!
+    @IBOutlet weak var p2Name: UILabel!
+    @IBOutlet weak var p2Wins: UILabel!
     
     var game = Game()
     
@@ -29,23 +35,13 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     
-        labelp2.transform = labelp2.transform.rotated(by: .pi)
+        p2Background.transform = p2Background.transform.rotated(by: .pi)
         
-        
-
-        setLabels()
-        
-        
-        
+       setLabels()
         
     }
     
 
-    
-
-
-    
-    
     
     @IBAction func buttonPress(_ sender: UIButton) {
         
@@ -74,52 +70,72 @@ class ViewController: UIViewController {
             do {}
         }
         
-        
         if game.makeAMove(position: position) == true {
             sender.alpha = 0.7
             DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
                 sender.alpha = 1
             }
             sender.isUserInteractionEnabled = false
-            sender.backgroundColor = getPlayerColor()
+            sender.backgroundColor = getPlayerColor(player: game.currentPlayer)
             
             if game.checkForWin() == true {
-                labelp1.text = "\(game.currentPlayer.name) won!"
-                labelp2.text = "\(game.currentPlayer.name) won!"
+                p1Label.font = UIFont.systemFont(ofSize: 40.0)
+                p2Label.font = UIFont.systemFont(ofSize: 40.0)
+                p1Label.text = "\(game.currentPlayer.name) won!"
+                p2Label.text = "\(game.currentPlayer.name) won!"
+                disableButtons()
                 
             }else if game.checkForDraw() == true{
-                labelp1.text = "Draw!"
-                labelp2.text = "Draw!"
+                p1Label.font = UIFont.systemFont(ofSize: 40.0)
+                p2Label.font = UIFont.systemFont(ofSize: 40.0)
+                p1Label.text = "Draw!"
+                p2Label.text = "Draw!"
                 
             }else{
                 game.nextPlayer()
                 setLabels()
             }
-            
-            
         }
+    }
     
+    
+    func disableButtons() {
+        button1.isUserInteractionEnabled = false
+        button1.isUserInteractionEnabled = false
+        button1.isUserInteractionEnabled = false
+        button1.isUserInteractionEnabled = false
+        button1.isUserInteractionEnabled = false
+        button1.isUserInteractionEnabled = false
+        button1.isUserInteractionEnabled = false
+        button1.isUserInteractionEnabled = false
+        button1.isUserInteractionEnabled = false
     }
     
     
     func setLabels() {
+        p1Background.backgroundColor = getPlayerColor(player: game.player1)
+        p1Name.text = game.player1.name
+        p1Wins.text = "\(game.player1.wins) wins"
+        
+        p2Background.backgroundColor = getPlayerColor(player: game.player2)
+        p2Name.text = game.player2.name
+        p2Wins.text = "\(game.player2.wins) wins"
+        
         if game.player1.nr == game.currentPlayer.nr {
-            labelp1.text = "Your turn!"
+            p1Label.font = UIFont.systemFont(ofSize: 40.0)
+            p1Label.text = "Your turn!"
+            p2Label.font = UIFont.systemFont(ofSize: 20.0)
+            p2Label.text = "Please wait for other player"
         }else{
-            labelp1.text = game.player1.name
+            p1Label.font = UIFont.systemFont(ofSize: 20.0)
+            p1Label.text = "Please wait for other player"
+            p2Label.font = UIFont.systemFont(ofSize: 40.0)
+            p2Label.text = "Your turn!"
         }
-        
-        if game.player2.nr == game.currentPlayer.nr {
-            labelp2.text = "Your turn!"
-        }else{
-            labelp2.text = game.player2.name
-        }
-        
     }
     
     
-    func getPlayerColor() -> UIColor {
-        let player = game.currentPlayer
+    func getPlayerColor(player: Player) -> UIColor {
         let color = UIColor(red: CGFloat(player.color[0]),
                             green: CGFloat(player.color[1]),
                             blue: CGFloat(player.color[2]),
